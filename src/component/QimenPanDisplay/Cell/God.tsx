3470995bce787e0b6ddd1a1640518e05750a18f0
bobbyleechiayih-art <bobby.lee.chia.yih@gmail.com>
2026-05-19 11:49:45 +0800

    Update God.tsx

--- src/component/QimenPanDisplay/Cell/God.tsx
+++ src/component/QimenPanDisplay/Cell/God.tsx
@@ -27,7 +27,7 @@ export const God = React.memo<Props>(({panSize, value, isScoreMode, highlight}) 
             <Flex direction="column" align="center" justify="center" lineHeight="1">
                 <span style={{ fontWeight: "bold" }}>{value || "　"}</span>
                 {value && (
-                    <span style={{ fontSize: "0.6em", opacity: 0.6, fontWeight: "normal", marginTop: "4px" }}>
+                    <span style={{ fontSize: "0.4em", opacity: 0.6, fontWeight: "normal", marginTop: "2px" }}>
                         {PinyinTranslationMap[value]}
                     </span>
                 )}
